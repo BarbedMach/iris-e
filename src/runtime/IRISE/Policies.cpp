6d72167b2541dc0ac56dbe3bbe3246612771697e
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-14 12:15:13 +0300

    Lots of cursed things but too late to go back now.

--- src/runtime/IRISE/Policies.cpp
+++ src/runtime/IRISE/Policies.cpp
@@ -1,27 +1,28 @@
 #include "Policies.hpp"
 
 namespace irise {
+    // Refer to iris/rt/iris_runtime.h. I inlined them to save an #include that was giving me issues. F this code base.
     std::string policyToString(int policy) {
         switch (policy) {
-            case iris_default:    return "default";
-            case iris_cpu:        return "cpu";
-            case iris_nvidia:     return "nvidia";
-            case iris_amd:        return "amd";
-            case iris_gpu_intel:  return "gpu intel";
-            case iris_gpu:        return "gpu";
-            case iris_phi:        return "phi";
-            case iris_fpga:       return "fpga";
-            case iris_dsp:        return "dsp";
-            case iris_roundrobin: return "roundrobin";
-            case iris_depend:     return "depend";
-            case iris_data:       return "data";
-            case iris_profile:    return "profile";
-            case iris_random:     return "random";
-            case iris_pending:    return "pending";
-            case iris_any:        return "any";
-            case iris_custom:     return "custom";
+            case (1 << 5):    return "default";
+            case (1 << 6):        return "cpu";
+            case (1 << 7):     return "nvidia";
+            case (1 << 8):        return "amd";
+            case (1 << 9):  return "gpu intel";
+            case ((1 << 7) | (1 << 8) | (1 << 9)):        return "gpu";
+            case (1 << 10):        return "phi";
+            case (1 << 11):       return "fpga";
+            case ((1 << 12)):        return "dsp";
+            case (1 << 18): return "roundrobin";
+            case (1 << 19):     return "depend";
+            case (1 << 20):       return "data";
+            case (1 << 21):    return "profile";
+            case (1 << 22):     return "random";
+            case (1 << 23):    return "pending";
+            case (1 << 24):        return "any";
+            case (1 << 27):     return "custom";
             default: break;
         }
-        return policy & iris_all ? "all" : "?";
+        return policy & (1 << 25) ? "all" : "?";
     }
 }
