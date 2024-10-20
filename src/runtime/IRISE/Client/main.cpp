9f6778ecd978408c14bd5949924f6ccb9d340868
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-20 12:58:18 +0300

    client updates

--- src/runtime/IRISE/Client/main.cpp
+++ src/runtime/IRISE/Client/main.cpp
@@ -15,20 +15,28 @@ auto main() -> int {
     while (true) {
         // Attempt to reconnect before each message send
         // Send a message to the server (e.g., HELLO)
-        client.sendMessage(irise::Message{irise::MessageType::HELLO});
+        try {
+            client.sendMessage(irise::Message{irise::MessageType::HELLO});
+        } catch(const std::exception& e) {
+            std::cout << "Error sending message. Details: " << e.what() << std::endl;
+        }
+        
 
         // Wait for a response in a loop
         while (true) {
-            auto response = client.receiveMessage();
-            if (!response.empty()) {
-                std::cout << "Response from server: " << response << std::endl;
-                break; // Exit the inner loop if we received a message
+            try {
+                auto response = client.receiveMessage();
+                if (!response.empty()) {
+                    std::cout << "Response from server: " << response << std::endl;
+                    break; // Exit the inner loop if we received a message
+                }
+            } catch(const std::exception& e) {
+                std::cout << "Error receiving message. Details: " << e.what() << std::endl;
             }
+            
             std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait before trying again
         }
-
-        // Optional: Add a delay before sending the next message
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 
     std::cout << "-------" << std::endl;
