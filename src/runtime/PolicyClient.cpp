6eb92a87d65b12f930282c7d57bea596a9864539
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-22 11:16:17 +0300

    bug fix

--- src/runtime/PolicyClient.cpp
+++ src/runtime/PolicyClient.cpp
@@ -19,7 +19,14 @@ auto PolicyClient::GetDevices(Task* task, Device** devs, int* ndevs) -> void {
         irise::Server::instance().sendMappingForKernelPending(irise::PendingMapping{ *it }).waitForACK();
 
         auto& deviceToUse = irise::Scheduler::instance().getMapping()[*it];
-        devs[0] = devs[deviceToUse.devNo];
+
+        std::cout << "Device to use: " << deviceToUse.devNo << std::endl;
+
+        if (!IsKernelSupported(task, devs_[deviceToUse.devNo])) {
+            return policies->GetPolicy(iris_default, NULL)->GetDevices(task, devs, ndevs);
+        }
+
+        devs[0] = devs_[deviceToUse.devNo];
         *ndevs = 1;
     } else {
         return policies->GetPolicy(iris_default, NULL)->GetDevices(task, devs, ndevs);
