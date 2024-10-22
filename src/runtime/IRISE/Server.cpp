936abfd90dd9ebaa2420825625cbbd05ad006621
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-22 12:43:27 +0300

    message synchronization problems

--- src/runtime/IRISE/Server.cpp
+++ src/runtime/IRISE/Server.cpp
@@ -28,7 +28,7 @@ auto Server::handleClient() -> void {
     while(true) {
         auto clientMessage = readFromClient();
         if (clientMessage.empty()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
             break;
         }
 
