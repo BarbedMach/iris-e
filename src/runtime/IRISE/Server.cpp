712ce82249747022a5b2022c9b8ce5c08baae5fd
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-10-23 11:50:37 +0300

    i accidentally introduced a bug in the server read from client code. reverted that in thtis commit

--- src/runtime/IRISE/Server.cpp
+++ src/runtime/IRISE/Server.cpp
@@ -4,16 +4,16 @@ namespace irise {
 
 auto Server::readFromClient() -> std::string {
     std::string incomingMessage{};
-    char buffer[2048];
+    char buffer[256];
 
-    ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1);
-    
-    if (bytesRead <= 0 && (errno = EAGAIN || errno == EWOULDBLOCK)) {
-        std::cout << "Server: Error reading from client!" << std::endl;
+    while(true) {
+        ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1);
+        if (bytesRead <= 0 && (errno = EAGAIN || errno == EWOULDBLOCK)) {
+            break;
+        }
+        incomingMessage.append(buffer, bytesRead);
     }
-    
-    incomingMessage.append(buffer, bytesRead);
-    
+
     return incomingMessage;
 }
 
